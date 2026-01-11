e999ac17886710ee088a80540cab55705e3fdbb3
LukasDD82 <lukas.simek.de@gmail.com>
2026-01-11 23:29:44 +0100

    Class Humans + Aliens is Finish

--- JUnitTesting/src/TestAliens.java
+++ JUnitTesting/src/TestAliens.java
@@ -1,2 +1,22 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class TestAliens {
+
+    private  int a = 100;
+    private  int b = 50;
+    private  int c = 24;
+
+    Aliens al = new Aliens();
+
+    @Test
+
+    public void TestAliens () {
+
+        assertEquals(0, al.health(a));
+        assertEquals(70, al.health(b));
+        assertEquals(80, al.health(c));
+
+    }
 }
--- src/Aliens.java
+++ src/Aliens.java
@@ -1,3 +1,4 @@
+
 public class Aliens extends Entities {
 
     private int poison;
@@ -10,6 +11,33 @@ public class Aliens extends Entities {
         this.poison = poison;
         this.bite = bite;
     }
+
+    public Aliens() {
+        super("Default", 0, 100);
+        this.poison = 0;
+        this.bite = 0;
+    }
+
+    @Override
+    public int getDamage() {
+        return poison + bite;
+    }
+
+
+    public int health(int damage) {
+
+        if (damage == 100)
+            return 0;
+        else if (damage < 100 && damage > 25 ) {
+            return 70;
+        } else if (damage <= 25) {
+            return 80;
+        }
+        else {
+            throw new IllegalArgumentException("Unsupported damage: " + damage);
+        }
+    }
+
 }
 
 
